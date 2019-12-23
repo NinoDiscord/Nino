352ad27983cde1009d73bde440c189d3e57c0b53
dondish <odedshapira1@gmail.com>
2019-12-24 00:08:13 +0200

    forgot lint

--- src/structures/managers/PunishmentManager.ts
+++ src/structures/managers/PunishmentManager.ts
@@ -97,7 +97,8 @@ export default class PunishmentManager {
     if (!settings) return [];
 
     let warnings = await this.bot.warnings.get(member.guild.id, member.id);
-    if (!warnings) warnings = this.bot.warnings.create(member.guild.id, member.id);
+    if (!warnings)
+      warnings = this.bot.warnings.create(member.guild.id, member.id);
     else
       await this.bot.warnings.update(member.guild.id, member.id, {
         amount: Math.min(warnings.amount + 1, 5),
