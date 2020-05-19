edf310ca299626fad7aa82cf6c467ef5178764e0
dondish <odedshapira1@gmail.com>
2020-05-19 19:26:24 +0300

    lint

--- src/structures/Language.ts
+++ src/structures/Language.ts
@@ -118,7 +118,6 @@ export default class Language {
   lazy_translate(translation: Translation): string {
     return this.translate(translation.key, translation.args);
   }
-  
 
   private _translate(translated: string, args?: { [x: string]: string }) {
     const KEY_REGEX = /[$]\{\s*([\w\.]+)\}\s*/g;
