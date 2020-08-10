25edb71fc4a04c51479c9565537c3c0e48ebf834
Noel <augu@voided.pw>
2020-08-10 08:11:54 -0700

    Remove bigTimeout tests

--- src/util/Utilities.test.ts
+++ src/util/Utilities.test.ts
@@ -59,41 +59,6 @@ describe('Utilities', () => {
     expect(embed).toStrictEqual(strings.join('\n'));
   });
 
-  it('should test bigTimeout with a 1 minute timeout', () => {
-    const MINUTE = 60000;
-    const callback = jest.fn(); // Creates a mock function
-
-    util.bigTimeout(callback, MINUTE);
-    expect(callback).not.toBeCalled();
-
-    jest.advanceTimersByTime(60000);
-    expect(callback).toBeCalled();
-    expect(callback).toHaveBeenCalledTimes(1);
-  });
-
-  it('should test bigTimeout with a 1 year timeout', () => {
-    const YEAR = 31557600000;
-    let temp = YEAR;
-    const callback = jest.fn();
-  
-    util.bigTimeout(callback, YEAR);
-    expect(callback).not.toBeCalled();
-
-    let i = 0;
-    while (temp > 0x7fffffff) {
-      jest.advanceTimersByTime(0x7fffffff);
-      temp -= 0x7fffffff;
-      i++;
-    }
-    jest.runOnlyPendingTimers();
-
-    expect(i).toBe(Math.floor(YEAR / 0x7fffffff));
-    expect(jest.getTimerCount()).toBe(0);
-    expect(0x7fffffff*i + temp).toBe(YEAR);
-    expect(callback).toBeCalled();
-    expect(callback).toHaveBeenCalledTimes(1);
-  });
-
   it('should return "Ban" when converting to "ban"', () =>
     expect(util.firstUpper('ban')).toBe('Ban')
   );
