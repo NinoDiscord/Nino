35f6c054ea665f87f632eb1443cc2bd7e48718e0
August <august@augu.dev>
2020-10-03 23:52:59 -0700

    Add Sentry tags, update dependencies

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,35 @@
+import { init, configureScope } from '@sentry/node';
 import Bot, { Config } from './structures/Bot';
+import { commitHash } from './util';
 import container from './inversify.config';
 import { TYPES } from './types';
-import { init } from '@sentry/node';
+import Logger from './structures/Logger';
 
 const pkg: any = require('../package.json');
 const config = container.get<Config>(TYPES.Config);
 const bot = container.get<Bot>(TYPES.Bot);
+const logger = new Logger();
 
 if (!config.sentryDSN) {
-  console.warn('WARNING: Missing "sentryDSN" in the "config.yml" file! This is optional to add but recommnended!');
+  logger.warn('WARNING: Missing "sentryDSN" in the "config.yml" file! This is optional to add but is recommnended!');
 } else {
   init({
     dsn: config.sentryDSN,
-    release: `${pkg.version}`,
+    release: `${pkg.version} (${commitHash})`,
+    environment: config.environment || 'development'
   });
+
+  configureScope(scope => {
+    scope.setTags({
+      'project.environment': config.environment,
+      'project.version': pkg.version,
+      'project.commit': commitHash,
+      'system.user': require('os').userInfo().username,
+      'system.os': process.platform
+    });
+  });
+
+  logger.info('Installed Sentry successfully, now building Nino...');
 }
 
 bot.build()
