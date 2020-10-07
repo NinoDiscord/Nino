7ca7297d7229e333b47b636f41067df9eaf64cf7
August <august@augu.dev>
2020-10-07 03:40:29 -0700

    Fix variables

--- src/structures/automod/Dehoisting.ts
+++ src/structures/automod/Dehoisting.ts
@@ -31,7 +31,7 @@ export default class AutoModDehoist {
 
     const settings = await this.bot.settings.get(m.guild.id);
     if (!settings || !settings.automod.dehoist) return;
-    if (m.user.username >= '0') return;
+    if (name >= '0') return;
 
     if (
       !PermissionUtils.above(me, m) || 
