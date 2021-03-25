7cb29046551b13fe9a085b6e8f80e3a5b652de19
Noel <cutie@floofy.dev>
2021-03-24 18:27:56 -0700

    work on v0 -> v1 script

--- scripts/v0-to-v1.js
+++ scripts/v0-to-v1.js
@@ -21,12 +21,17 @@
  */
 
 const { LoggerWithoutCallSite } = require('tslog');
+const { createConnection } = require('typeorm');
+const { parse } = require('@augu/dotenv');
+const { exec } = require('child_process');
+const { join } = require('path');
 const fs = require('fs/promises');
 
 const logger = new LoggerWithoutCallSite({
   displayFunctionName: true,
   exposeErrorCodeFrame: true,
   displayInstanceName: true,
+  displayFilePath: false,
   instanceName: 'script: v0 -> v1'
 });
 
@@ -40,6 +45,19 @@ const getFlags = (content) => {
   return record;
 };
 
+const execAsync = (command, options) => new Promise((resolve, reject) => {
+  const child = exec(command, options);
+
+  child.stdout.on('data', (chunk) => logger.debug(`stdout (${command}):`, chunk));
+  child.stderr.on('data', (chunk) => logger.debug(`stderr (${command}):`, chunk));
+  child.on('exit', (code, signal) => {
+    const success = code === 0;
+    logger.info(`${command}: Command has exited with code ${code}${signal ? `, with signal '${signal}'` : ''}`);
+
+    return success ? resolve() : reject(new Error('Read logs with `stderr` prefixed.'));
+  });
+});
+
 (async() => {
   logger.info('Welcome to the database conversion script!');
   logger.info('This script takes care of converting the Mongo database to the PostgreSQL one!');
@@ -49,32 +67,63 @@ const getFlags = (content) => {
   const flags = getFlags(args.join(' '));
   const directory = flags['dir'] || flags['d'];
 
-  if (directory !== undefined && typeof directory === 'boolean') {
-    logger.fatal('`--dir` / `-d` flag must be specified.');
-    process.exit(1);
-  } else if (directory !== undefined) {
-    await loadInBsonDatabase(directory);
-  } else {
-    await loadInFromMongo();
+  if (!directory) {
+    logger.warn('Assuming we should load in from Mongo itself...');
+    const config = parse({
+      delimiter: ',',
+      populate: false,
+      file: join(__dirname, '..', '.env')
+    });
+
+    try {
+      await import('mongodb');
+    } catch(ex) {
+      logger.info('Assuming you ran this script once, now installing...');
+
+      await installMongo();
+      logger.info('This script will exit, please re-run it using `npm run migrate`!');
+      process.exit(0);
+    }
+
+    logger.info('Assuming you reloaded the script, now converting...');
+    const { default: mongodb } = await import('mongodb');
+    const client = new mongodb.MongoClient(config.MONGO_URI, {
+      appname: 'Nino: v0 -> v1',
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    });
+
+    try {
+      await client.connect();
+      logger.info('Connected to MongoDB!');
+    } catch(ex) {
+      logger.error('Unable to connect to Mongo:', ex);
+      process.exit(1);
+    }
+
+    const db = client.db(config.MONGO_DB);
+    const guilds = db.collection('guilds');
+    const cases = db.collection('cases');
+    const users = db.collection('users');
+    const warnings = db.collection('warnings');
+
+    logger.info('Now connecting to PostgreSQL...');
+    const connection = await createConnection();
+
+    logger.info('Connected to PostgreSQL!');
+
+    await connection.close();
+    await client.close();
+
+    process.exit(0);
   }
 })();
 
-const loadInBsonDatabase = async (directory) => {
-  /** @type {import('fs').Stats} */
-  let stats;
+const installMongo = async () => {
   try {
-    await fs.lstat(directory);
+    await execAsync('npm i mongodb');
   } catch(ex) {
-    logger.fatal('Unable to get statistics of the directory specified, is it an absolute path?');
+    logger.error(ex);
     process.exit(1);
   }
-
-  if (!stats.isDirectory()) {
-    logger.fatal('Specified "directory" was not a directory.');
-    process.exit(1);
-  }
-};
-
-const loadInFromMongo = async() => {
-  // todo: this uwu
 };
