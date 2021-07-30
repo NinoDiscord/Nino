fa313ddbbd05d327cf71a611b6548ace1c5222e4
Noel <cutie@floofy.dev>
2021-07-30 08:08:00 -0700

    e

--- src/main.ts
+++ src/main.ts
@@ -76,7 +76,6 @@ const ReconnectCodes = [
 
 const OtherPossibleReconnectCodes = [
   'WebSocket was closed before the connection was established',
-  // eslint-disable-next-line prettier/prettier
   'Server didn\'t acknowledge previous heartbeat, possible lost connection'
 ];
 
